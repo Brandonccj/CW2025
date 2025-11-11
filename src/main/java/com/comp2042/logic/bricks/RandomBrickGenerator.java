e82dbd4b02bd44ee619628d3eddc411e93bc39b8
Brandonccj <brandonccj@gmail.com>
2025-11-11 12:53:59 +0800

    Implemented 7-bag randomizer for brick generator for fairer gameplay

--- src/main/java/com/comp2042/logic/bricks/RandomBrickGenerator.java
+++ src/main/java/com/comp2042/logic/bricks/RandomBrickGenerator.java
@@ -2,14 +2,14 @@ package com.comp2042.logic.bricks;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Deque;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
+
 
 public class RandomBrickGenerator implements BrickGenerator {
 
     private final List<Brick> brickList;
-
     private final Deque<Brick> nextBricks = new ArrayDeque<>();
 
     public RandomBrickGenerator() {
@@ -21,20 +21,29 @@ public class RandomBrickGenerator implements BrickGenerator {
         brickList.add(new SBrick());
         brickList.add(new TBrick());
         brickList.add(new ZBrick());
-        nextBricks.add(brickList.get(ThreadLocalRandom.current().nextInt(brickList.size())));
-        nextBricks.add(brickList.get(ThreadLocalRandom.current().nextInt(brickList.size())));
+        fillBag();
+        fillBag();
+    }
+
+    private void fillBag() {
+        List<Brick> bag = new ArrayList<>(brickList);
+        Collections.shuffle(bag);
+        nextBricks.addAll(bag);
     }
 
     @Override
     public Brick getBrick() {
-        if (nextBricks.size() <= 1) {
-            nextBricks.add(brickList.get(ThreadLocalRandom.current().nextInt(brickList.size())));
+        if (nextBricks.size() <= 7) {
+            fillBag();
         }
         return nextBricks.poll();
     }
 
     @Override
     public Brick getNextBrick() {
+        if (nextBricks.isEmpty()) {
+            fillBag();
+        }
         return nextBricks.peek();
     }
 }
